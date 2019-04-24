06a23c18f3a55a2247b66997ba71743b41553e0e
Jennifer Oh <ohjennifer.90@gmail.com>
2019-04-24 16:57:54 -0400

    oo-cash-register

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,40 @@
+class CashRegister
 
+  attr_accessor :total, :discount, :quantity, :items, :price
+
+  def initialize(discount = nil)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+
+  def add_item(title, price, quantity = 1)
+    @price = price
+
+    @total += @price * quantity
+    if quantity > 1
+      counter = 0
+      while counter < quantity
+        @items << title
+        counter += 1
+      end
+    else
+      @items << title
+    end
+  end
+
+  def apply_discount
+    if @discount == nil
+      "There is no discount to apply."
+    else @total -= (@total * @discount/100)
+      "After the discount, the total comes to $#{@total}."
+    end
+  end
+
+  def void_last_transaction
+    @total -= @price
+  end
+
+
+end
