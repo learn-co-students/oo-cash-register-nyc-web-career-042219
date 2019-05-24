84dde748656fd9ab2f6cb2f0773eb7a1b6e52daf
github username <github email address>
2019-05-24 09:14:45 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,33 +1,42 @@
 require "pry"
 class CashRegister
+attr_accessor :total, :discount
 
-attr_accessor :total, :discount, :items
 
-def initialize(discount = 0)
-  @total = 0
-  @discount = discount
-  @items = []
-  @receipt = []
-end
 
-def add_item(title, price, quantity = 1)
-  (1..quantity).each {|item| @items.push(title)}
-  @total += price * quantity
-  @receipt << [title, price]
-end
-
-def apply_discount
-  if discount > 0
-    @total = total - (discount * total / 100)
-    "After the discount, the total comes to $#{@total}."
-  else
-    "There is no discount to apply."
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
   end
-end
 
-def void_last_transaction
-  @receipt.last
-  @total -= @receipt.last[-1]
-end
+  def add_item(title, price, quantity = 1)
+    @items.push([title, price, quantity, @discount])
+    @total += price * quantity
+  end
+
+  def apply_discount
+    if discount > 0
+      @total = @total - (@total * discount / 100)
+      "After the discount, the total comes to $#{@total}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def items
+    item_titles = []
+    @items.each do |item|
+      item[2].times {item_titles << item[0]}
+    end
+    item_titles
+  end
+
+  def void_last_transaction
+    price = @items[-1][1]
+    quantity = @items[-1][2]
+    @total = @total - (price * quantity)
+    @items.pop
+  end
 
 end
